586b109eca46c092a1cb7aa27f67a873b84989e7
rio <riogoodman@gmail.com>
2017-03-21 23:23:41 -0700

    http buffer with no pkg

--- program.js
+++ program.js
@@ -1,6 +1,13 @@
 const http = require('http')
 http.get(process.argv[2], response => {
   response.setEncoding('utf8')
-  response.on('data', console.log
+  let chars = '';
+  response.on('data', data => {
+    chars += data
+  })
+  response.on('end', () => {
+    console.log(chars.length)
+    console.log(chars)
+  })
   response.on('error', console.error)
 }).on('error', console.error)
