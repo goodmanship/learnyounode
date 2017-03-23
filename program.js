bcadd83e891c39c73a1d7f67cb65849d67627b0e
rio <riogoodman@gmail.com>
2017-03-22 23:17:29 -0700

    second to the last one done

--- program.js
+++ program.js
@@ -1,9 +1,9 @@
-const fs = require('fs')
 const http = require('http')
+const map = require('through2-map')
 
 function listener(req, res) {
-  const src = fs.createReadStream(process.argv[3])
-  src.pipe(res)
+  if (req.method !== 'POST') return res.end('send me a POST\n')
+  req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res)
 }
 const server = http.createServer(listener)
 server.listen(process.argv[2])
