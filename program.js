bf932403cda94da17c5e27c5461d18a317226a08
rio <riogoodman@gmail.com>
2017-03-22 21:40:51 -0700

    async juggling done

--- program.js
+++ program.js
@@ -1,10 +1,15 @@
 const http = require('http')
-const bl = require('bl')
-http.get(process.argv[2], response => {
-  response.pipe(bl((err, data) => {
-    if (err) console.log(err)
-    data = data.toString()
-    console.log(data.length)
-    console.log(data)
-  }))
-}).on('error', console.error)
+let responses = ['', '', '']
+let done = [false, false, false]
+function hitIt(url, i) {
+  http.get(url, response => {
+    response.setEncoding('utf8')
+    response.on('error', console.error)
+    response.on('data', data => responses[i] += data)
+    response.on('end', () => {
+      done[i] = true
+      if (done.every(x => x)) responses.forEach(r => console.log(r))
+    })
+  })
+}
+process.argv.slice(2).forEach(hitIt)
