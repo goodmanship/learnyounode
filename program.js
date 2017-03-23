98c8aa99c77f733c3a1467328cd1759fe37d1241
rio <riogoodman@gmail.com>
2017-03-22 22:00:26 -0700

    time server done

--- program.js
+++ program.js
@@ -1,15 +1,9 @@
-const http = require('http')
-let responses = ['', '', '']
-let done = [false, false, false]
-function hitIt(url, i) {
-  http.get(url, response => {
-    response.setEncoding('utf8')
-    response.on('error', console.error)
-    response.on('data', data => responses[i] += data)
-    response.on('end', () => {
-      done[i] = true
-      if (done.every(x => x)) responses.forEach(r => console.log(r))
-    })
-  })
+const net = require('net')
+const strftime = require('strftime')
+
+function listener(socket) {
+  const date = strftime('%Y-%m-%d %H:%M', new Date())
+  socket.end(date + '\n')
 }
-process.argv.slice(2).forEach(hitIt)
+const server = net.createServer(listener)
+server.listen(process.argv[2])
