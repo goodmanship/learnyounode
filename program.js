6f12341ac907b264783602bb0c6c12f23b570b75
rio <riogoodman@gmail.com>
2017-03-23 09:00:47 -0700

    doneeee

--- program.js
+++ program.js
@@ -1,9 +1,32 @@
 const http = require('http')
-const map = require('through2-map')
+const url = require('url')
+
+function parsetime(d) {
+  return {
+    'hour': d.getHours(),
+    'minute': d.getMinutes(),
+    'second': d.getSeconds()
+  }
+}
+
+function unixtime(d) {
+  return {
+    'unixtime': d.getTime()
+  }
+}
 
 function listener(req, res) {
-  if (req.method !== 'POST') return res.end('send me a POST\n')
-  req.pipe(map(chunk => chunk.toString().toUpperCase())).pipe(res)
+  if (req.method !== 'GET') return res.end('send me a GET and I\'ll tell you\n')
+  const reqUrl = url.parse(req.url, true)
+  let d = new Date(reqUrl.query['iso'])
+  if (reqUrl.pathname == '/api/parsetime') {
+    r = parsetime(d)
+  } else if (reqUrl.pathname == '/api/unixtime') {
+    r = unixtime(d)
+  }
+  res.writeHead(200, { 'Content-Type': 'application/json' })
+  res.end(JSON.stringify(r))
 }
+
 const server = http.createServer(listener)
 server.listen(process.argv[2])
